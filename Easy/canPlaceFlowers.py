5bbdb19c9f670f8e07f82e5357cc1faa607e39a3
Isaiah Ghansam <isaiahg2@umbc.edu>
2026-08-22 20:36:14 -0400

    canPlaceFlowers - These darn edge cases lol

--- Easy/canPlaceFlowers.py
+++ Easy/canPlaceFlowers.py
@@ -3,20 +3,33 @@ from typing import List
 
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        count = 0
+
+        if n == 0:
+            return True
+        
+        for i in range(len(flowerbed)):
+            if i == 0:
+                left = 0
+            else:
+                left = flowerbed[i-1]
+
+            if i == len(flowerbed) - 1:
+                right = 0
+            else:
+                right = flowerbed[i+1]
+
+            if flowerbed[i] == 0 and left == 0 and right == 0:
+                flowerbed[i] = 1
+                count +=1
+
+            if count >= n:
+                return True
+            
+        return False
 
         
 
-        if flowerbed[0] == 0:
-            flowerbed[0] = 1
-
-        for i in range(len(flowerbed)):
-            for j in range(i+1,len(flowerbed)):
-                if flowerbed[i] == 1 and flowerbed[j] == 1:
-                    tf = False
-                else:
-                    tf = True
-
-        return tf
 
 
 
